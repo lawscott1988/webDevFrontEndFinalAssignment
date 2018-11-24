a7be06502f13e2165f4ed886fc969c37d221799b
lawscott1988 <lawscott1988@googlemail.com>
2018-11-24 14:32:13 +0000

    Create common.js

---
+++ js/common.js
@@ -0,0 +1,41 @@
+function extendMenu() {
+    var x = document.getElementById("mainMenu");
+    if (x.className === "collapsible") {
+        x.className += " responsive";
+    } else {
+        x.className = "collapsible";
+    }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+// forward and previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("pictureSlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace("active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+}
